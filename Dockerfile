d0fac04c9980c0531e65bf389f093c9c2b35a1c5
Reik Loeber <reik.loeber@gmail.com>
2025-09-18 11:56:44 +0200

    adaption api route

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+ARG NODE_VERSION=20.17.0
+
+FROM node:${NODE_VERSION}-alpine AS base
+
+WORKDIR /app
+##############################################
+FROM base AS build
+
+COPY . .
+
+RUN npm install && npm run build -- --configuration production
+##############################################
+FROM nginx:latest
+
+COPY --from=build /app/dist/angular-conduit /usr/share/nginx/html
+COPY default.conf /etc/nginx/conf.d
--- src/app/core/interceptors/api.interceptor.ts
+++ src/app/core/interceptors/api.interceptor.ts
@@ -6,6 +6,7 @@ import {
   HttpRequest,
 } from "@angular/common/http";
 import { Observable } from "rxjs";
+import {environment} from "../../../environments/environment";
 
 @Injectable({ providedIn: "root" })
 export class ApiInterceptor implements HttpInterceptor {
@@ -13,7 +14,7 @@ export class ApiInterceptor implements HttpInterceptor {
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    const apiReq = req.clone({ url: `https://api.realworld.io/api${req.url}` });
+    const apiReq = req.clone({ url: `${environment.apiurl}/api${req.url}` });
     return next.handle(apiReq);
   }
 }
